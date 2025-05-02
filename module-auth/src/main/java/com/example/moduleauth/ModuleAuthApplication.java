c84eebf1b4ecbd6ba4d9092a7f4479827a4c6524
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-05-02 17:54:59 +0900

    🛠️ infra : 유레카 클라이언트 추가1

--- module-auth/src/main/java/com/example/moduleauth/ModuleAuthApplication.java
+++ module-auth/src/main/java/com/example/moduleauth/ModuleAuthApplication.java
@@ -2,8 +2,10 @@ package com.example.moduleauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients("com.example.moduleauth.feign")
 public class ModuleAuthApplication {
