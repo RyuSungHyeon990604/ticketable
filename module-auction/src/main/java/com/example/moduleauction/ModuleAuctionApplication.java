02605f334b3456c83e86fbaef6ada8d0e48bba10
KyujungLee <leekyujung123@gmail.com>
2025-05-02 11:10:20 +0900

    ♻️ Refactor: common 의존성 제거

--- module-auction/src/main/java/com/example/moduleauction/ModuleAuctionApplication.java
+++ module-auction/src/main/java/com/example/moduleauction/ModuleAuctionApplication.java
@@ -2,10 +2,8 @@ package com.example.moduleauction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@ComponentScan(basePackages = {"com.example.moduleauction", "com.example.modulecommon"})
 @EnableJpaAuditing
 @SpringBootApplication
 public class ModuleAuctionApplication {
