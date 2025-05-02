0254fdf0da5d17a2255c35c4e4baf40b1e2f8409
seaking110 <seaking110@naver.com>
2025-05-02 11:47:50 +0900

    ♻️ Refactor : MSA 도입에 따른 공통 모듈 정보 wating 모듈로 도입 및 import 수정 및 코드 정리

---
+++ module-waiting/src/main/java/com/example/modulewaiting/global/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.example.modulewaiting.global.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+	private final String status;
+	private final String message;
+	private final String code;
+	private final Map<String, String> errors;
+
+	public ErrorResponse(String status, String message, String code) {
+		this.status = status;
+		this.message = message;
+		this.code = code;
+		this.errors = new HashMap<String, String>();
+	}
+}
