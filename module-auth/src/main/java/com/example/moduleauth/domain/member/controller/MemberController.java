f87dde5e053549ed542dbaeeb1163043d815a7a6
cheoni5417 <cheoni5417@naver.com>
2025-05-02 09:37:53 +0900

    ♻️ Refactor: memberId PathVariable로 받아오는거 수정

--- module-auth/src/main/java/com/example/moduleauth/domain/member/controller/MemberController.java
+++ module-auth/src/main/java/com/example/moduleauth/domain/member/controller/MemberController.java
@@ -15,21 +15,21 @@ public class MemberController {
 	
 	private final MemberService memberService;
 	
-	@PatchMapping("/v1/members/{memberId}")
+	@PatchMapping("/v1/members")
 	public ResponseEntity<String> updatePassword(
-		@PathVariable Long memberId,
+		@LoginUser AuthUser authUser,
 		@Valid @RequestBody UpdatePasswordRequest request
 	) {
-		memberService.updatePassword(memberId, request);
+		memberService.updatePassword(authUser.getMemberId(), request);
 		return ResponseEntity.ok("비밀번호가 변경되었습니다.");
 	}
 	
-	@DeleteMapping("/v1/members/{memberId}")
+	@DeleteMapping("/v1/members")
 	public ResponseEntity<String> deleteMember(
-		@PathVariable Long memberId,
+		@LoginUser AuthUser authUser,
 		@Valid @RequestBody DeleteMemberRequest request
 	) {
-		memberService.deleteMember(memberId, request);
+		memberService.deleteMember(authUser.getMemberId(), request);
 		return ResponseEntity.ok("멤버가 삭제되었습니다.");
 	}
 }
