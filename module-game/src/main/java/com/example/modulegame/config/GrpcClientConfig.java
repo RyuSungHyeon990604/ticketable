0c7581bd8648884200858c544826c6ec410c3590
AirIHL <inhak1122@gmail.com>
2025-05-05 23:34:21 +0900

    🐛 Fix: grpc 설정 추가

--- module-game/src/main/java/com/example/modulegame/config/GrpcClientConfig.java
+++ module-game/src/main/java/com/example/modulegame/config/GrpcClientConfig.java
@@ -3,19 +3,23 @@ package com.example.modulegame.config;
 import com.example.grpc.ticket.TicketServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GrpcClientConfig {
+    @Value("${grpc.host}")
+    private String host;
 
+    @Value("${grpc.port}")
+    private int port;
     @Bean
     public TicketServiceGrpc.TicketServiceBlockingStub ticketServiceBlockingStub() {
         ManagedChannel channel = ManagedChannelBuilder
-                .forAddress("localhost", 9092) // 티켓 서버 주소/포트
+                .forAddress(host, port) // 티켓 서버 주소/포트
                 .usePlaintext() // TLS 없이 통신
                 .build();
         return TicketServiceGrpc.newBlockingStub(channel);
     }
 }
-
