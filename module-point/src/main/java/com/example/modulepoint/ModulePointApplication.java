da81c3a74bf4e8406ca77c600bff9cdc97864273
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-05-05 20:26:10 +0900

    🛠️ infra : 포인트 서비스 재배포

--- module-point/src/main/java/com/example/modulepoint/ModulePointApplication.java
+++ module-point/src/main/java/com/example/modulepoint/ModulePointApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//포인트 재배포
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ModulePointApplication {
